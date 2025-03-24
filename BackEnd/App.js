0e65889cd5b7bebe901b79ba829c470396f230bf
Mohamed Hosam <mohamed.hosam2310@gmail.com>
2025-03-24 19:19:01 +0200

    enable cors

--- BackEnd/App.js
+++ BackEnd/App.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
 const courseRouter = require('./Routes/courseRoutes');
 const testimonialRouter = require('./Routes/testimonialRoutes');
 const internRouter = require('./Routes/internRoutes');
 const userRouter = require('./Routes/userRoutes');
 const blogRouter = require('./Routes/blogRoutes');
+
 const app = express();
+
+app.use(cors());
+
 
 app.use(morgan('dev'));
 app.use(express.json());
-
-
 
 app.use("/courses", courseRouter);
 app.use("/testimonials", testimonialRouter);
