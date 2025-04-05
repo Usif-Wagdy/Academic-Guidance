2dadac9e086fc39842e0430833bd2a39e3cd8fe5
Mohamed Hosam <mohamed.hosam2310@gmail.com>
2025-04-05 22:54:43 +0200

    Added cv , interns Admin

--- BackEnd/Routes/internRoutes.js
+++ BackEnd/Routes/internRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const internController = require('../Controllers/interController');
-const { authMiddleware, allwedTo } = require('../Middlewares/authMiddleware');
+const { authMiddleware, allwedTo, allowedTo } = require('../Middlewares/authMiddleware');
 
 router.get('/', internController.getAllInterns);
 router.get('/:id', internController.getInternById);
-router.post('/', authMiddleware, internController.createIntern);
+router.post('/', authMiddleware, allowedTo('superAdmin', 'cvAdmin'), internController.createIntern);
 router.delete('/:id', authMiddleware, internController.deleteIntern);
-router.patch('/:id', authMiddleware, internController.updateIntern);
+router.patch('/:id', authMiddleware, allowedTo('superAdmin', 'cvAdmin'), internController.updateIntern);
 
 module.exports = router;
