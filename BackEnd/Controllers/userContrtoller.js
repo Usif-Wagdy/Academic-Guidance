5c60e60d7cdf0a02bb845c4d12f64133dd997e49
Mohamed Hosam <mohamed.hosam2310@gmail.com>
2025-04-09 22:52:16 +0200

    Added Make Admin Endpoint

--- BackEnd/Controllers/userContrtoller.js
+++ BackEnd/Controllers/userContrtoller.js
@@ -209,3 +209,43 @@ exports.getUser = async (req, res) => {
     res.status(500).json({ error: 'Failed to retrieve user' });
   }
 }
+
+
+
+
+exports.addRole = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { role, isAdmin } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ error: 'Invalid user ID format' });
+    }
+
+    if (!role) {
+      return res.status(400).json({ error: 'Role is required' });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        role,
+        isAdmin,
+      },
+      { new: true },
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json({
+      message: 'Role added successfully',
+      user: updatedUser,
+    });
+  }
+  catch (error) {
+    console.error('Error adding role:', error.message);
+    res.status(500).json({ error: 'Failed to add role' });
+  }
+}
--- BackEnd/Routes/userRoutes.js
+++ BackEnd/Routes/userRoutes.js
@@ -8,18 +8,21 @@ const { imageUpload } = require('../Config/cloudinaryConfig');
 router.post('/signUp', authController.addUser);
 router.post('/signIn', authController.login);
 
+router.post('/role/:id', authMiddleware, allowedTo('superAdmin'), userController.addRole);
+
+router.get('/', authMiddleware, allowedTo('superAdmin'), userController.getAllUsers);
+router.get('/:id', authMiddleware, allowedTo('superAdmin'), userController.getUser);
+router.get('/getStudents', authMiddleware, allowedTo('superAdmin'), userController.getStudents);
+router.get('/getInstructors', authMiddleware, allowedTo('superAdmin'), userController.getInstructors);
+
+router.delete('/:id', authMiddleware, allowedTo('superAdmin'), userController.deleteUser);
+router.patch('/update-user/:id', authMiddleware, userController.updateUser);
+
+
 router.patch(
   '/update-image/:id',
   imageUpload.single('photo'),
   userController.addImage
 );
-router.patch('/update-user/:id', authMiddleware, userController.updateUser);
-router.get('/', authMiddleware, allowedTo('superAdmin'), userController.getAllUsers);
-router.get('/:id', authMiddleware, allowedTo('superAdmin'), userController.getUser);
-
-router.get('/getStudents', authMiddleware, allowedTo('superAdmin'), userController.getStudents);
-router.get('/getInstructors', authMiddleware, allowedTo('superAdmin'), userController.getInstructors);
-router.delete('/:id', authMiddleware, allowedTo('superAdmin'), userController.deleteUser);
-
 
 module.exports = router;
