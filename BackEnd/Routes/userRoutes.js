f326aa83df3a04ed9707d085fba3a9b72837df17
Abdelrahman Mohy <a.mohiyyy@gmail.com>
2025-04-21 21:34:31 +0200

    Simple fix

--- BackEnd/Routes/userRoutes.js
+++ BackEnd/Routes/userRoutes.js
@@ -1,36 +1,84 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../Controllers/userContrtoller');
+const userController = require('../Controllers/userController');
 const authController = require('../Controllers/authController');
-const { authMiddleware, allowedTo } = require('../Middlewares/authMiddleware');
-const { imageUpload } = require('../Config/cloudinaryConfig');
+const {
+  authMiddleware,
+  allowedTo,
+} = require('../Middlewares/authMiddleware');
+const {
+  imageUpload,
+} = require('../Config/cloudinaryConfig');
 router.post('/signUp', authController.addUser);
 router.post('/signIn', authController.login);
 
-router.post('/role/:id', authMiddleware, allowedTo('superAdmin'), userController.addRole);
+router.post(
+  '/role/:id',
+  authMiddleware,
+  allowedTo('superAdmin'),
+  userController.addRole,
+);
 
-router.get('/check-auth', authMiddleware, userController.checkAuth);
+router.get(
+  '/check-auth',
+  authMiddleware,
+  userController.checkauth,
+);
 
-router.post('/check-password', authMiddleware, userController.checkPassword);
+router.post(
+  '/check-password',
+  authMiddleware,
+  userController.checkPassword,
+);
 
-router.get('/', authMiddleware, allowedTo('superAdmin'), userController.getAllUsers);
-router.get('/getStudents', authMiddleware, allowedTo('superAdmin'),
-  userController.getStudents);
-router.get('/getInstructors', authMiddleware, allowedTo('superAdmin'),
-  userController.getInstructors);
-router.get('/:id', authMiddleware, allowedTo('superAdmin'),
-  userController.getUser);
+router.get(
+  '/',
+  authMiddleware,
+  allowedTo('superAdmin'),
+  userController.getAllUsers,
+);
+router.get(
+  '/getStudents',
+  authMiddleware,
+  allowedTo('superAdmin'),
+  userController.getStudents,
+);
+router.get(
+  '/getInstructors',
+  authMiddleware,
+  allowedTo('superAdmin'),
+  userController.getInstructors,
+);
+router.get(
+  '/:id',
+  authMiddleware,
+  allowedTo('superAdmin'),
+  userController.getUser,
+);
 
-router.delete('/:id', authMiddleware, allowedTo('superAdmin'), userController.deleteUser);
-router.patch('/update-user/:id', authMiddleware, userController.updateUser);
+router.delete(
+  '/:id',
+  authMiddleware,
+  allowedTo('superAdmin'),
+  userController.deleteUser,
+);
+router.patch(
+  '/update-user/:id',
+  authMiddleware,
+  userController.updateUser,
+);
 
-router.patch('/update-password', authMiddleware, userController.updatePassword);
+router.patch(
+  '/update-password',
+  authMiddleware,
+  userController.updatePassword,
+);
 
 router.patch(
   '/update-image/:id',
   imageUpload.single('photo'),
-  userController.addImage
+  userController.addImage,
 );
 
 module.exports = router;
